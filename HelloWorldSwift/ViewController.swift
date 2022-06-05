4efcc05576e7d8ac87e8d8aba4edf8f0bcb0e94c
Hryhorii Mosia <greg.mosia@gmail.com>
2022-06-05 14:34:56 +0300

    Initial Commit

---
+++ HelloWorldSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldSwift
+//
+//  Created by Gregory Mosia on 05.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
