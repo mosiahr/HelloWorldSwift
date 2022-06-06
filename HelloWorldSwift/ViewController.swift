a92aa2645d86d9fa9e3dea8f79086f8d55f21601
Hryhorii Mosia <greg.mosia@gmail.com>
2022-06-06 21:59:31 +0300

    Add hide/show text logic

--- HelloWorldSwift/ViewController.swift
+++ HelloWorldSwift/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        helloWorldLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func makeButtonAction() {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            toggleButton.setTitle("Hide text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            toggleButton.setTitle("Show text", for: .normal)
+        }
+    }
+    
 }
 
